20260110737ecbcd4f842c9de3afdae9770e42fb
Miriam Mueller <mail@miriam-mueller.com>
2024-03-17 11:03:14 +0100

    update mock data & reverse todos so open todos are at the top

--- svelteApp/src/routes/todos/+page.svelte
+++ svelteApp/src/routes/todos/+page.svelte
@@ -4,17 +4,17 @@
 
 <h1>ToDos</h1>
 <div class="d-grid gap-2">
-{#each data.todos as todo}
+{#each data.todos.reverse() as todo}
     <div class="list-group ">
-        <label class="list-group-item py-2">
-            <input class="form-check-input flex-shrink-0" type="checkbox" value="">
-            <span>
-                { todo.status }
-                <small class="d-block text-body-secondary">
-                    { todo.text }
-                </small>
-            </span>
+        <div class="list-group-item">
+        <span class="d-block">
+            { todo.text }
+        </span>
+        <label>
+            <input class="form-check-input flex-shrink-0" type="checkbox" checked="{todo.status === 'done'}">
+            { todo.status }
         </label>
+        </div>
     </div>
 {/each}
 </div>
