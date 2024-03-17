86f86654c5d021fe7bea61e800a41e49636de35d
Miriam Mueller <mail@miriam-mueller.com>
2024-03-17 14:47:30 +0100

    use latest bootstrap and also import js

--- svelteApp/src/routes/todos/+page.svelte
+++ svelteApp/src/routes/todos/+page.svelte
@@ -3,18 +3,23 @@
 </script>
 
 <h1>ToDos</h1>
-<div class="d-grid gap-2">
-{#each data.todos.reverse() as todo}
-    <div class="list-group ">
-        <div class="list-group-item">
+
+<div class="list-group gap-2">
+    {#each data.todos as todo}
+        <div class="list-group-item d-flex">
         <span class="d-block">
             { todo.text }
         </span>
-        <label>
-            <input class="form-check-input flex-shrink-0" type="checkbox" checked="{todo.status === 'done'}">
-            { todo.status }
-        </label>
+            <label>
+                <input class="form-check-input flex-shrink-0" type="checkbox" checked="{todo.status === 'done'}">
+                { todo.status }
+            </label>
+
+            <button class="btn btn-outline-secondary" type="button">Edit</button>
+            <button class="btn btn-primary btn-danger" type="button">Delete</button>
+
         </div>
-    </div>
-{/each}
+
+    {/each}
 </div>
+
