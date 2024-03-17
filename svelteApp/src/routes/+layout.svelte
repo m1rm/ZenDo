59bfc4f5d3f434da74e163d790a4aafb24bb8958
Miriam Mueller <mail@miriam-mueller.com>
2024-03-17 10:53:46 +0100

    add basic footer and its pages; cleanup styles

--- svelteApp/src/routes/+layout.svelte
+++ svelteApp/src/routes/+layout.svelte
@@ -1,11 +1,11 @@
-<nav class="navbar navbar-expand-md navbar-dark bg-dark" aria-label="Fourth navbar example">
+<nav class="navbar navbar-expand-md navbar-dark bg-dark">
     <div class="container-fluid">
         <a class="navbar-brand" href="#">ZenDo</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#nav" aria-controls="nav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
 
-        <div class="collapse navbar-collapse" id="navbarsExample04">
+        <div class="collapse navbar-collapse" id="nav">
             <ul class="navbar-nav me-auto mb-2 mb-md-0">
                 <li class="nav-item">
                     <a class="nav-link" href="/">Home</a>
@@ -24,3 +24,16 @@
 <div class="container-fluid">
     <slot />
 </div>
+
+<footer class="container-fluid d-flex flex-wrap justify-content-between align-items-center my-4 border-top">
+    <div class="d-flex align-items-center">
+        <span class="mb-3 mb-md-0 text-body-secondary">© 2024 ZenDo</span>
+    </div>
+
+    <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
+        <li class="ms-3"><a class="text-body-secondary" href="/about">About</a></li>
+        <li class="ms-3"><a class="text-body-secondary" href="/imprint">Imprint</a></li>
+        <li class="ms-3"><a class="text-body-secondary" href="privacy">Privacy</a></li>
+    </ul>
+</footer>
+
