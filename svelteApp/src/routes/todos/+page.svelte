932d51a2cb11547c4b920b1b176912fb2278051f
Miriam Mueller <mail@miriam-mueller.com>
2024-03-17 15:18:21 +0100

    fix layout using row/col/flex

--- svelteApp/src/routes/todos/+page.svelte
+++ svelteApp/src/routes/todos/+page.svelte
@@ -3,23 +3,24 @@
 </script>
 
 <h1>ToDos</h1>
-
-<div class="list-group gap-2">
+<div class="row m-2 gap-2">
     {#each data.todos as todo}
-        <div class="list-group-item d-flex">
-        <span class="d-block">
+        <div class="col-12 col-sm-6 border d-flex flex-column">
+            <span class="mt-2">
             { todo.text }
-        </span>
-            <label>
-                <input class="form-check-input flex-shrink-0" type="checkbox" checked="{todo.status === 'done'}">
-                { todo.status }
-            </label>
+            </span>
 
-            <button class="btn btn-outline-secondary" type="button">Edit</button>
-            <button class="btn btn-primary btn-danger" type="button">Delete</button>
-
+            <div class="d-flex justify-content-between align-items-end gap-2 mb-2">
+                <label>
+                    <input class="form-check-input flex-shrink-0" type="checkbox" checked="{todo.status === 'done'}">
+                    { todo.status }
+                </label>
+                <div>
+                    <button class="btn btn-sm btn-outline-secondary" type="button">Edit</button>
+                    <button class="btn btn-sm btn-primary btn-danger" type="button">Delete</button>
+                </div>
+            </div>
         </div>
 
     {/each}
 </div>
-
