04762297a3cb2348c1341cc53a036bfeab6a589f
Miriam Mueller <mail@miriam-mueller.com>
2024-03-23 19:50:53 +0100

    add route for single todo

--- goApi/main.go
+++ goApi/main.go
@@ -4,15 +4,16 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
 )
 
 type Todo struct {
-	id     int32
-	text   string
-	status string
+	Id     int32  `json:"id"`
+	Text   string `json:"text"`
+	Status string `json:"status"`
 }
 
-func todos(w http.ResponseWriter, req *http.Request) {
+func getTodos(w http.ResponseWriter, req *http.Request) {
 	todos := []Todo{
 		{1, "Style mock content", "done"},
 		{2, "Style nav", "done"},
@@ -37,8 +38,44 @@ func todos(w http.ResponseWriter, req *http.Request) {
 	w.Write(response)
 }
 
+func getTodo(w http.ResponseWriter, req *http.Request) {
+	queryParam := req.PathValue("id")
+	id, err := strconv.Atoi(queryParam)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("error parsing id query param, %v", err), http.StatusInternalServerError)
+		return
+	}
+
+	// bc. array index starts at 0
+	correctedId := id - 1
+
+	todos := []Todo{
+		{1, "Style mock content", "done"},
+		{2, "Style nav", "done"},
+		{3, "Add header and footer", "done"},
+		{4, "Add CTAs without function", "done"},
+		{5, "Add Go Api with mock responses", "open"},
+		{6, "dockerize", "open"},
+		{7, "add just & docker compose setup", "open"},
+		{8, "add DB", "open"},
+		{9, "replace mock API responses data with real data", "open"},
+		{10, "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea", "open"},
+	}
+
+	response, err := json.Marshal(todos[correctedId])
+	if err != nil {
+		http.Error(w, fmt.Sprintf("error building the response, %v", err), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(response)
+}
+
 func main() {
-	http.HandleFunc("GET /todos", todos)
+	http.HandleFunc("GET /todos", getTodos)
+	http.HandleFunc("GET /todos/{id}", getTodo)
 
 	http.ListenAndServe(":8090", nil)
 }
