4b2aadef73ce38b73e9c08346786a467b2ca6012
Miriam Mueller <mail@miriam-mueller.com>
2024-09-01 11:49:29 +0200

    adjust mock data to project status quo

--- migrations/001_createTodoTable.sql
+++ migrations/001_createTodoTable.sql
@@ -9,14 +9,11 @@ CREATE TABLE todos (
 INSERT INTO todos
 (description, status)
 VALUES
-    ('Style FE', '1' ),
-    ('Add CTAs w/o functionality', '1' ),
-    ('dockerize dev', '1' ),
-    ('replace mock API responses data with real data', '1' ),
-    ('Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea', 0 ),
-    ('improve project (goApi) structure', '0' ),
-    ('implement missing API functionality', '0' ),
-    ('make CTAs in FE functional', '0' ),
-    ('add APIdoc', '0' ),
-    ('find and use migration tooling', '0' ),
-    ('prod setup', '0' );
+    ('Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea', 1 ),
+    ('implement edit todo in API', '0' ),
+    ('implement edit todo in FE', '0' ),
+    ('add status change functionality in FE', '0'),
+    ('improve project structure (goApi)', '0' ),
+    ('improve project structure (svelteKit app)', '0' ),
+    ('add pagination BE', '0'),
+    ('add pagination FE', '0');
