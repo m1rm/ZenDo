145d0c5b53b4ef319ca9bee3f9f7ede4ba868d25
Miriam Mueller <mail@miriam-mueller.com>
2024-10-05 12:58:24 +0200

    show dialog only on failure since the change is visible in the ui in the success case anyways; dialog has no added value for user in success case

--- sveltekit/src/routes/todos/+page.svelte
+++ sveltekit/src/routes/todos/+page.svelte
@@ -1,45 +1,36 @@
 <script>
-    import { onMount } from 'svelte'
-    import { todoData } from '../../stores/todos'
+    import { onMount } from "svelte";
+    import { todoData } from "../../stores/todos";
 
     onMount(async () => {
         try {
             const response = await fetch("http://localhost:8090/todos");
             if (!response.ok) {
-                throw new Error('Network response was not ok');
+                throw new Error("Network response was not ok");
             }
             const data = await response.json();
             todoData.set(data);
         } catch (error) {
-            console.error('Fetch error:', error);
+            console.error("Fetch error:", error);
             todoData.set([]);
         }
-    })
+    });
 
     /**
-     * @param {boolean} success
      * @param {string} action
-     * @param {Response} response
+     * @param {string} cause
      */
-    async function builtConfirmationMessage(success, action, response) {
-        if (!success) {
-            confirmationMessage = `Failed to ${action} todo: ${response.statusText}`
-            showConfirmation = true
-            console.log(await response.json())
-        } else {
-            confirmationMessage = `${action} todo: success.`
-                textInput = ''; // Reset the form input
-                showConfirmation = true;
-
-                // Clear the confirmation message after 1 second
-                setTimeout(() => {
-                    showConfirmation = false
-                }, 1000);
-        }
+    async function builtErrorMessage(action, cause) {
+        errorMessage = `Failed to ${action} todo: ${cause}`;
+        showConfirmation = true;
+        // Clear the confirmation message after 1 second
+        setTimeout(() => {
+            showConfirmation = false;
+        }, 1000);
     }
 
-    let textInput = '';
-    let confirmationMessage = '';
+    let textInput = "";
+    let errorMessage = "";
     let showConfirmation = false;
 
     /**
@@ -49,23 +40,26 @@
         event.preventDefault();
 
         try {
-            const response = await fetch('/api/submit', {
-                method: 'POST',
+            const response = await fetch("/api/submit", {
+                method: "POST",
                 headers: {
-                    'Content-Type': 'application/json'
+                    "Content-Type": "application/json",
                 },
-                body: JSON.stringify({ description: textInput })
+                body: JSON.stringify({ description: textInput }),
             });
-            
-            await builtConfirmationMessage(response.ok, 'submit', response);
+
             if (!response.ok) {
-                throw new Error('Network response was not ok');
+                await builtErrorMessage("submit", "API response was not ok.");
+                throw new Error(
+                    "API response was not ok when submitting new todo.",
+                );
             } else {
-            const data = await response.json();
-            todoData.update(currentData => [...currentData, data]);  
+                const data = await response.json();
+                todoData.update((currentData) => [...currentData, data]);
             }
         } catch (error) {
-            console.error('Error:', error);
+            await builtErrorMessage("submit", "An unexpected error occured.");
+            console.error("Error:", error);
         }
     }
 
@@ -75,44 +69,66 @@
     async function deleteTodo(id) {
         try {
             const response = await fetch(`http://localhost:8090/todos/${id}`, {
-                method: 'DELETE'
+                method: "DELETE",
             });
-
-            await builtConfirmationMessage(response.ok, 'delete', response);
+            console.log("response: ", response);
+            console.log("response.ok: ", response.ok);
             if (!response.ok) {
-                throw new Error('Failed to delete the todo');
+                await builtErrorMessage("delete", "API response was not ok.");
+                throw new Error("API response was not ok when deleting todo.");
             } else {
-                todoData.update(currentData => currentData.filter(todo => todo.id !== id));
+                todoData.update((currentData) =>
+                    currentData.filter((todo) => todo.id !== id),
+                );
             }
         } catch (error) {
-            console.error('Error:', error);
+            await builtErrorMessage("delete", "An unexpected error occured.");
+            console.error("Error:", error);
         }
     }
-
 </script>
 
 <span class="h1 col-12">ToDos</span>
 
 {#if showConfirmation}
-<div style="z-index: 11" class="position-fixed top-50 start-50 translate-middle">
-    <div class="toast show bg-info" role="alert" aria-live="assertive" aria-atomic="true">
-        <div class="toast-header">
-            <strong class="me-auto">Info</strong>
-            <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
-        </div>
-        <div class="toast-body">
-            {confirmationMessage}
-            <div id="confirmationCountdownBar"></div>
+    <div
+        style="z-index: 11"
+        class="position-fixed top-50 start-50 translate-middle"
+    >
+        <div
+            class="toast show bg-info"
+            role="alert"
+            aria-live="assertive"
+            aria-atomic="true"
+        >
+            <div class="toast-header">
+                <strong class="me-auto">Info</strong>
+                <button
+                    type="button"
+                    class="btn-close"
+                    data-bs-dismiss="toast"
+                    aria-label="Close"
+                ></button>
+            </div>
+            <div class="toast-body">
+                {errorMessage}
+                <div id="confirmationCountdownBar"></div>
+            </div>
         </div>
     </div>
-</div>
 {/if}
 
 <div class="row m-2 gap-2">
     <form on:submit={handleSubmit}>
         <div class="form-group mb-1 col-6">
             <label for="textInput">Enter Text</label>
-            <input type="text" class="form-control" id="textInput" bind:value={textInput} placeholder="Enter text">
+            <input
+                type="text"
+                class="form-control"
+                id="textInput"
+                bind:value={textInput}
+                placeholder="Enter text"
+            />
         </div>
         <button type="submit" class="btn btn-success">Submit</button>
     </form>
@@ -120,20 +136,30 @@
     {#each $todoData as todo}
         <div class="col-12 col-sm-6 border d-flex flex-column">
             <span class="mt-2 mb-2">
-            { todo.description }
+                {todo.description}
             </span>
 
-            <div class="d-flex justify-content-between align-items-end gap-2 mb-2">
+            <div
+                class="d-flex justify-content-between align-items-end gap-2 mb-2"
+            >
                 <label>
-                    <input class="form-check-input flex-shrink-0" type="checkbox" checked="{todo.status === 1}">
-                    { todo.status === 1 ? 'done' : 'open' }
+                    <input
+                        class="form-check-input flex-shrink-0"
+                        type="checkbox"
+                        checked={todo.status === 1}
+                    />
+                    {todo.status === 1 ? "done" : "open"}
                 </label>
                 <div>
-                    <button class="btn btn-sm btn-outline-secondary" type="button">Edit</button>
-                    <button 
+                    <button
+                        class="btn btn-sm btn-outline-secondary"
+                        type="button">Edit</button
+                    >
+                    <button
                         class="btn btn-sm btn-primary btn-danger"
                         type="button"
-                        on:click={() => deleteTodo(todo.id)}>
+                        on:click={() => deleteTodo(todo.id)}
+                    >
                         Delete
                     </button>
                 </div>
@@ -144,22 +170,21 @@
 
 <style>
     #confirmationCountdownBar {
-    position: absolute;
-    bottom: 0;
-    left: 0;
-    width: 100%;
-    height: 5px; /* Height of the progress bar */
-    background-color: #4caf50; /* Color of the progress bar */
-    animation: decrease 1s linear forwards;
-}
-
-@keyframes decrease {
-    from {
+        position: absolute;
+        bottom: 0;
+        left: 0;
         width: 100%;
+        height: 5px; /* Height of the progress bar */
+        background-color: #4caf50; /* Color of the progress bar */
+        animation: decrease 1s linear forwards;
     }
-    to {
-        width: 0;
-    }
-}
 
+    @keyframes decrease {
+        from {
+            width: 100%;
+        }
+        to {
+            width: 0;
+        }
+    }
 </style>
