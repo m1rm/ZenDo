5ca9c06cca3b53792eec8df1847a0f58e63df768
Miriam Mueller <mail@miriam-mueller.com>
2024-10-05 15:48:40 +0200

    refactor: use todo type

--- sveltekit/src/stores/todos.js
+++ sveltekit/src/stores/todos.js
@@ -1,18 +1,12 @@
-import { writable } from 'svelte/store'
-
-/**
- * @typedef {Object} Todo
- * @property {number} id - The unique identifier for the todo item.
- * @property {string} description - The description of the todo item.
- * @property {number} status - The status of the todo. 0 = open, 1 = done
- */
+import { writable } from "svelte/store";
+/// <reference path="../types.d.ts" />
 
 /** @type {import('svelte/store').Writable<Todo[]>} */
-export const todoData = writable([])
+export const todoData = writable([]);
 
 /** @type {import('svelte/store').Writable<Todo>} */
 export const selectedTodo = writable({
     id: 0,
-    description: '',
-    status: 0
-  });
+    description: "",
+    status: 0,
+});
